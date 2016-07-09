3e7296058400643a81792604a71ce49575d1967b
dubbinary <dubbinary@gmail.com>
2016-07-09 20:46:21 +0300

    ssl remove from server start

--- test_auth.py
+++ test_auth.py
@@ -2,15 +2,17 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-url_get = "http://localhost:5000/import"
-url_post = "http://localhost:5000/export"
+# host = "localhost"
+host = "ssl-bs-auth.herokuapp.com"
+url_get = "https://%s:5000/import" % host
+url_post = "https://%s:5000/export" % host
 
 user = "admin"
 password = "secret"
 
 headers = {'Authorization': 'Basic YWRtaW46c2VjcmV0'}   ##Scrapped auth
 
-res = requests.get(url_get, auth=HTTPBasicAuth(user, password))
+res = requests.get(url_get, auth=HTTPBasicAuth(user, password), verify='keys/server.crt')
 print "Status: ",res.status_code
 print "Text: ",res.text
 try:
