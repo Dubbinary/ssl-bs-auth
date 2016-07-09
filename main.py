36391744a682bce61c4981a4797e963b96d89440
dubbinary <dubbinary@gmail.com>
2016-07-09 13:41:13 +0300

    base of server

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, url_for
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def getIndex():
+    return render_template('index.html')
+
+@app.route('/import/', methods=['GET'])
+def do_import():
+    return "On import link"
+
+@app.route('/export/', methods=['POST'])
+def do_export():
+    return "On export link"
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
+    
